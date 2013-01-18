6b4730289743a6bed82947f8106fc2dab7aa897b
kashanmasud <kashanmasud@hotmail.com>
2013-01-18 11:16:45 -0500

    Revert "version1 of templates"
    This reverts commit 9e391af727462a24da174de9c0181344e507537b.

--- views-view-field--features-blocks--upcoming-events--field-date-time-rep.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	//Displays the Month and Day Only for the Date field of the Upcoming Events View
-	$event_date = explode(" ", $output, 3); // break up field output based off spaces
-	print '<div class="event_date_month">' . $event_date[0] . '</div><div class="event_date_day">' . $event_date[1] . '</div>'; //display month and day with assigned class names
-?>
